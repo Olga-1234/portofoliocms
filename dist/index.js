8e5bb7c8bcce3404f8bcadab9dfbbde260c63da3
Olga Mujinga <monaolgita@gmail.com>
2021-05-21 12:13:59 +0200

    feat: add console log uuid

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,14 @@
+/* eslint-disable no-undef */
 
+var _uuid = _interopRequireDefault(require("uuid"));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
 var sum = function sum(number1, number2) {
   return number1 + number2;
 };
 
 console.log(sum(4, 5));
+console.log(_uuid.default.v4);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import uuid from "uuid";
+
 const sum = (number1, number2) => number1 + number2;
 console.log(sum(4, 5));
+console.log(uuid.v4);
