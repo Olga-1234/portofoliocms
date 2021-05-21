44da03c35feed719f71b5745ed2383436885ad85
Olga Mujinga <monaolgita@gmail.com>
2021-05-21 12:00:38 +0200

    feat: sum

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+
+
+var sum = function sum(number1, number2) {
+  return number1 + number2;
+};
+
+console.log(sum(4, 5));
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const sum = (number1, number2) => number1 + number2;
+console.log(sum(4, 5));
